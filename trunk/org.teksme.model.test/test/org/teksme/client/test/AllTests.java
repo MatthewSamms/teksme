ca8f9a56b3e0f1b10417ada3933d94791c5ac6b6
fabianoc <fabianoc@e97005b1-721c-3801-859e-9ff44725afab>
2010-06-17 14:54:37 +0000

    initial commit teksme test / examples project

---
+++ trunk/org.teksme.model.test/test/org/teksme/client/test/AllTests.java
@@ -0,0 +1,10 @@
+package org.teksme.client.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { TeksInquiryTest.class })
+public class AllTests {
+
+}
