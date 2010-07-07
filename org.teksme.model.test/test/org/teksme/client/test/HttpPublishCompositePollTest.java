734a2d4c609069c14be84d06ca8335b829726fd7
fabianoc <fabianoc@e97005b1-721c-3801-859e-9ff44725afab>
2010-07-07 02:38:52 +0000

    new testcase to publish a poll on the server

---
+++ org.teksme.model.test/test/org/teksme/client/test/HttpPublishCompositePollTest.java
@@ -0,0 +1,120 @@
+package org.teksme.client.test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HttpPublishCompositePollTest {
+
+	private static final String SERVICE_URL = "http://localhost:8080/org.teksme.server.web.api/createpoll";
+
+	private static final String MODEL_FILE = "output/teks.xml";
+
+	private static OutputStreamWriter out;
+
+	private static URLConnection connection = null;
+
+	@BeforeClass
+	public static void setResourceSet() {
+		try {
+
+			URL url = new URL(SERVICE_URL);
+			connection = url.openConnection();
+			connection.setDoOutput(true);
+			connection.setUseCaches(false);
+			connection.setRequestProperty("Content-type", "text/xml");
+			connection.setRequestProperty("Connection", "Keep-Alive");
+
+			out = new OutputStreamWriter(connection.getOutputStream());
+
+			System.out.println("prepering to send... ");
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void publishCompositePoll() {
+
+		// String fileContent =
+		// "<?xml version=\"1.0\" encoding=\"UTF-8\"?><WAV> this is the content</WAV>";
+
+		try {
+			String fileContentEncoded = URLEncoder.encode(
+					convertXMLFileToString(MODEL_FILE), "UTF-8");
+			out.write(fileContentEncoded);
+			out.write("Writing to XML file");
+
+			out.flush();
+			connection.connect();
+			out.close();
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
+
+			String decodedString;
+
+			while ((decodedString = in.readLine()) != null) {
+				System.out.println(decodedString);
+			}
+			in.close();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public String convertXMLFileToString(String fileName) {
+		try {
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
+					.newInstance();
+			InputStream inputStream = new FileInputStream(new File(fileName));
+			org.w3c.dom.Document doc = documentBuilderFactory
+					.newDocumentBuilder().parse(inputStream);
+			StringWriter stw = new StringWriter();
+			Transformer serializer = TransformerFactory.newInstance()
+					.newTransformer();
+			serializer.transform(new DOMSource(doc), new StreamResult(stw));
+			return stw.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
