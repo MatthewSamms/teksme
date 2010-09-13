0bded5cc7c5085abe2f4ac8aa746ca180e05b344
mayworm <mayworm@gmail.com>
2010-09-13 20:38:30 +0000

    keep the mq services

---
+++ server-side/org.teksme.server.distributed/src/org/teksme/server/distributed/Activator.java
@@ -0,0 +1,24 @@
+package org.teksme.server.distributed;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public class Activator implements BundleActivator {
+
+	private ServiceRegistration registration;
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+	}
+
+}
