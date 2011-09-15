0acdcabdcbf16c02c9b4a2b5f370bb31a3a4cd70
fabianoc <fabianoc@e97005b1-721c-3801-859e-9ff44725afab>
2011-09-15 05:19:03 +0000

    Refactoring

--- trunk/server-side/org.teksme.server.common/src/org/teksme/server/common/messaging/AMQPServiceRegistry.java
+++ trunk/server-side/org.teksme.server.common/src/org/teksme/server/common/messaging/AMQPServiceRegistry.java
@@ -1,6 +1,6 @@
 /**
- * Copyright 2010 TèksMe, Inc.
- * TèksMe licenses this file to you under the Apache License, version
+ * Copyright 2010 Creative Works, Inc.
+ * Creative Works licenses this file to you under the Apache License, version
  * 2.0 (the "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *    http://www.apache.org/licenses/LICENSE-2.0
